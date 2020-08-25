c8c3ba09e82acd0ee6b6eda7c7ea0c197ae13c4b
stefantrinh1 <stefantrinh1.work@gmail.com>
2020-08-24 21:18:31 -0600

    slider styles

--- src/components/General/Slider.js
+++ src/components/General/Slider.js
@@ -1,19 +1,52 @@
 import React from 'react'
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
-export default function SlideShow({images}) {
-    return (
-        <CarouselProvider
-        naturalSlideWidth={100}
-        naturalSlideHeight={100}
-        totalSlides={3}
-      >
-        
-        <Slider>
-            {images.map((slide, index)=> <Slide index={index}><Image src={slide} alt="slide" /></Slide>)}
-        </Slider>
-        <ButtonBack>Back</ButtonBack>
-        <ButtonNext>Next</ButtonNext>
-      </CarouselProvider>
-    )
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  Image,
+} from 'pure-react-carousel'
+import 'pure-react-carousel/dist/react-carousel.es.css'
+export default function SlideShow({ images }) {
+  return (
+    <CarouselProvider
+      naturalSlideWidth={150}
+      naturalSlideHeight={100}
+      totalSlides={4}>
+      <Slider>
+        {images.map((slide, index) => (
+          <Slide index={index}>
+            <Image
+              style={{
+                objectFit: 'cover',
+                height: 'auto',
+                objectPosition: 'center',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+              }}
+              isIntrinsicHeight={true}
+              isBgImage={true}
+              src={slide}
+              alt='slide'
+            />
+          </Slide>
+        ))}
+      </Slider>
+      <ButtonBack>Back</ButtonBack>
+      <ButtonNext>Next</ButtonNext>
+    </CarouselProvider>
+  )
 }
+
+// display: block;
+//     width: 100%;
+//     height: auto;
+//     object-position: center;
+//     object-fit: cover;
+//     position: absolute;
+//     top: 50%;
+//     left: 50%;
+//     transform: translate(-50%, -50%);
